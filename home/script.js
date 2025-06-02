598c6fe8d63a55f18be6d3adc3594be7293d05d7
Thibaut Tbc Bujiriri <166028783+thibaut-tbc-bujiriri@users.noreply.github.com>
2025-06-02 15:10:05 +0200

    update

--- home/script.js
+++ home/script.js
@@ -6,9 +6,9 @@ function(){
 // alert(" Bienvenu sur ma page de presentation...")
 // console.warn("Attention !!!");
 
-const camerafeed = document.getElementById('camerafeed');
-navigator.mediaDevices.getUserMedia({video: true}).
-then(stream => {
-    camerafeed.srcObject = stream;
-})
-.catch(err => {console.error("erreur d'acces a la camera :",err)});
+// const camerafeed = document.getElementById('camerafeed');
+// navigator.mediaDevices.getUserMedia({video: true}).
+// then(stream => {
+//     camerafeed.srcObject = stream;
+// })
+// .catch(err => {console.error("erreur d'acces a la camera :",err)});
